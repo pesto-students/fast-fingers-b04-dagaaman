085736141a7371ade761765671c3eb6dfb674467
dagaaman <50543027+dagaaman@users.noreply.github.com>
2020-10-26 14:47:28 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import Home from './Home/Home';
+import Player from './Player/Player';
+import Result from './Result/Result';
+
+import {Route, Switch } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fast Fingers </h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/play">
+            <Player />
+          </Route>
+          <Route path="/result">
+            <Result />
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
